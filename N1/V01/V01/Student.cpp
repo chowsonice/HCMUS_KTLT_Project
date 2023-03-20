c95b91fc85153642d48ef732efa2c059e94e3ebc
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-03-20 12:04:44 +0700

    Add default constructor and copy constructor of class Student

--- N1/V01/V01/Student.cpp
+++ N1/V01/V01/Student.cpp
@@ -1,4 +1,6 @@
 #include "Student.h"
+#include <string>
+using namespace std;
 
 Student::Student(int no, const char* id, const char* firstName, const char* lastName, char gender, const char* dateOfBirth, const char* socialID){
     this->no = no;
@@ -57,7 +59,17 @@ const char* Student::getSocialID() const {
 }
 
 ostream& operator<<(ostream& os, const Student& s) {
-    os << s.no << " " << s.studentID << " " << s.firstName << " " << s.gender << s.dateOfBirth << " " << s.socialID << endl;
+    os << to_string(s.no) << " ";
+    for (int i = 0; i < strlen(s.studentID); i++) os << s.studentID[i];
+    os << " ";
+    for (int i = 0; i < strlen(s.firstName); i++) os << s.firstName[i];
+    os << " ";
+    for (int i = 0; i < strlen(s.lastName); i++) os << s.lastName[i];
+    os << " ";
+    os << s.gender << " ";
+    for (int i = 0; i < strlen(s.dateOfBirth); i++) os << s.dateOfBirth[i];
+    os << " ";
+    for (int i = 0; i < strlen(s.socialID); i++) os << s.socialID[i];
     return os;
 }
 //const Scoreboard& Student::getScoreboard() const {
--- N1/V01/V01/Student.h
+++ N1/V01/V01/Student.h
@@ -15,7 +15,9 @@ private:
 	char* socialID;
 	//Scoreboard scoreboard;
 public:
+	Student() : no(0), studentID(nullptr), firstName(nullptr), lastName(nullptr), gender('X'), dateOfBirth(nullptr), socialID(nullptr) {}
 	Student(int no, const char* studentID, const char* firstName, const char* lastName, char gender, const char* dateOfBirth, const char* socialID);
+	Student(const Student& s) : Student(s.no, s.studentID, s.firstName, s.lastName, s.gender, s.dateOfBirth, s.socialID) {}
     ~Student();
 
     int getNo() const;
