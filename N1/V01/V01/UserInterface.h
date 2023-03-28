6d4ce3c897f10e20b876c4dedfbc98fd807d1b2b
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-03-28 12:51:07 +0700

    User Interface (not done)

---
+++ N1/V01/V01/UserInterface.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//void loginScreen() {
+//	char* usernameBuffer = new char[100];
+//	cout << "Username: ";
+//	cin >> usernameBuffer;
+//	cout << "Password: ";
+//	cin >> 
+//}
+
