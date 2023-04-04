1c86a36a1e75c9d1db6f150ed84bdf93163d596d
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-04-05 00:35:35 +0700

    add struct University, add function addClass and printListOfClasses

--- N1/V01/V01/University.h
+++ N1/V01/V01/University.h
@@ -1,5 +1,30 @@
 #pragma once
-class University
-{
+#include <list>
+#include "Class.h"
+using namespace std;
+
+struct University {
+	list<Class*> listOfClasses;
+public:
+	Student* findStudent(string id) {
+		for (Class *c : listOfClasses) {
+			Student *s = c->findStudent(id);
+			if (s != nullptr) return s;
+		}
+	}
+	void addClass() {
+		string buffer;
+		cout << "Enter class name: ";
+		cin >> buffer;
+		Class* newclass = new Class(buffer);
+		cout << "Importing students...\n";
+		newclass->importStudentsFromCSV();
+		listOfClasses.push_back(newclass);
+	}
+	void printListOfClasses() {
+		for (Class* c : listOfClasses) cout << *c << "\n";
+	}
 };
+
+
 
