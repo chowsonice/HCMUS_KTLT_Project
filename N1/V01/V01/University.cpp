dfead6ef87da282f98cf3d2afbcd96f81424c969
xdatus <111189200+xdatus@users.noreply.github.com>
2023-04-05 13:39:48 +0700

    fix chauu

--- N1/V01/V01/University.cpp
+++ N1/V01/V01/University.cpp
@@ -1,1 +1,28 @@
 #include "University.h"
+
+Student* University::findStudent(string id)
+{
+	for (Class* c : listOfClasses) {
+		Student* s = c->findStudent(id);
+		if (s != nullptr) return s;
+	}
+}
+
+void University::addClass()
+{
+
+	string buffer;
+	cout << "Enter class name: ";
+	cin >> buffer;
+	Class* newclass = new Class(buffer);
+	cout << "Importing students...\n";
+	newclass->importStudentsFromCSV();
+	listOfClasses.push_back(newclass);
+}
+
+void University::printListOfClasses()
+{
+
+	for (Class* c : listOfClasses) cout << *c << "\n";
+
+}
--- N1/V01/V01/University.h
+++ N1/V01/V01/University.h
@@ -6,24 +6,9 @@ using namespace std;
 struct University {
 	list<Class*> listOfClasses;
 public:
-	Student* findStudent(string id) {
-		for (Class *c : listOfClasses) {
-			Student *s = c->findStudent(id);
-			if (s != nullptr) return s;
-		}
-	}
-	void addClass() {
-		string buffer;
-		cout << "Enter class name: ";
-		cin >> buffer;
-		Class* newclass = new Class(buffer);
-		cout << "Importing students...\n";
-		newclass->importStudentsFromCSV();
-		listOfClasses.push_back(newclass);
-	}
-	void printListOfClasses() {
-		for (Class* c : listOfClasses) cout << *c << "\n";
-	}
+	Student* findStudent(string id);
+	void addClass();
+	void printListOfClasses();
 };
 
 
