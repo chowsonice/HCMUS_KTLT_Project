4c5641b8efc6d9f364d7d69b816873004481a2e9
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-04-17 00:26:32 +0700

    21CLC01's accounts

--- N1/V01/V01/V01.cpp
+++ N1/V01/V01/V01.cpp
@@ -10,30 +10,24 @@
 #include "Student.h"
 #include "University.h"
 
+
 int main()
 {
     Account main;
-    University uni;
-    LinkedList<SchoolYear*> years;
-    
+    Semester* cursem = new Semester();
     try {
         loginScreen(main);
-        change_passwordScreen(main);
+        //change_passwordScreen(main);
     }
     catch (const char* error) {
         cout << "ERROR: " << error << endl;
         return -1;
     }
-    if (main.getType() == 1) staffMenu();
+    if (main.getType() == 1) {
+        staffMenu();
+    }
     else if (main.getType() == 2) {
-
-        // T nghĩ là mình cần chỉnh lại một chút phần student
-        // Thông tin student t thấy là mình nên xuất ra thành file hết, rồi khi đăng nhập vào thì tìm file đó mà đọc thôi
-        // Vậy nên chắc mình thêm:
-        // 1. Hiếu - Student thêm phần, exportToText(), xuất thông tin student ra file, trong thư mục student_data/[MSSV].txt
-        // Student có thay đổi gì (update thông tin /  update scoreboard) gì thì phải xuất ra hết
-        // 2. XĐ - Thêm hàm ngoài Student* readStudentFromFile() để đọc thông tin student để cho phần StudentMenu
-        // Do xong 2 cái đó mới làm cái này được nên làm nhanh nha
+        studentMenu(main);
     }
     return 0;
 }
